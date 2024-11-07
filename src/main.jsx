6d6f69a88e50689cf2e7cccc37e0033d256387dc
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2024-11-07 23:07:02 +0530

    react-clerk-auth ✅

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+
+// Entry Point of Application 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react';
+import App from "./App"
+
+
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App/>
+    </ClerkProvider>
+  </React.StrictMode>,
+)
